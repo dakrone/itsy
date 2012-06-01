639c003d249b1f2c51d2d465f299af6dab772a46
Lee Hinman <lee@writequit.org>
2012-05-31 21:36:07 -0600

    bump to 0.1.1-SNAPSHOT

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject itsy "0.1.0"
+(defproject itsy "0.1.1-SNAPSHOT"
   :description "A threaded web-spider written in Clojure "
   :url "https://github.com/dakrone/itsy"
   :license {:name "Eclipse Public License"
