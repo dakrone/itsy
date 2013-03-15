e69f58bf50f76ebcf204e94ed07e36dd852f1513
Lee Hinman <lee@writequit.org>
2013-03-14 20:58:43 -0600

    bump clojure and clj-http deps

--- project.clj
+++ project.clj
@@ -3,8 +3,8 @@
   :url "https://github.com/dakrone/itsy"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.4.0"]
-                 [clj-http "0.5.7"]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clj-http "0.6.5"]
                  [com.cemerick/url "0.0.6"]
                  [org.clojure/tools.logging "0.2.3"]
                  [log4j "1.2.16"]
