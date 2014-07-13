98ccf1ba44d0337fda945af25b1b24a750b10ba9
Ben Sima <bensima@gmail.com>
2014-07-12 22:06:25 -0400

    Updated some dependency versions

--- project.clj
+++ project.clj
@@ -3,12 +3,12 @@
   :url "https://github.com/dakrone/itsy"
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
-  :dependencies [[org.clojure/clojure "1.5.1"]
-                 [clj-http "0.7.3"]
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [clj-http "0.9.2"]
                  [clj-robots "0.6.0"]
-                 [com.cemerick/url "0.0.8"]
-                 [org.clojure/tools.logging "0.2.6"]
-                 [log4j "1.2.17"]
+                 [com.cemerick/url "0.1.1"]
+                 [org.clojure/tools.logging "0.3.0"]
+                 [log4j "1.2.15"]
                  [org.slf4j/slf4j-api "1.7.5"]
                  [org.slf4j/slf4j-log4j12 "1.7.5"]
                  [org.slf4j/jcl-over-slf4j "1.7.5"]
